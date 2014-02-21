a386939a9ed03bc5d8066c438360b74c0ce488c5
Lutz Behnke <lutz.behnke@informatik.haw-hamburg.de>
2014-02-21 15:02:32 +0100

    add spec testing infrastructure

---
+++ spec/classes/mailman_spec.rb
@@ -0,0 +1,42 @@
+require 'spec_helper'
+
+describe 'mailman' do
+  let(:node) { 'testhost.example.org' }
+  let(:hiera_config) { 'spec/fixtures/hiera/hiera.yaml' }
+
+  context 'with default settngs' do
+    it do
+      should contain_package('mailman')
+
+      should contain_class('mailman')
+
+      should contain_service('mailman')
+     end
+  end
+
+  context 'with rich config' do
+    let(:params) { {
+      :default_url_host    => 'lists.example.org',
+      :default_email_host  => 'example.org',
+      :default_url_pattern => 'https://%s/mailman/',
+      :mailman_site_list   => 'mailman-list',
+      :mm_cfg_settings     => {
+        'ALLOW_SITE_ADMIN_COOKIES' => 'Yes',
+        'PUBLIC_ARCHIVE_URL' => "'https://%(hostname)s/pipermail/%(listname)s'",
+        'MTA' => "'Postfix'",
+        'POSTFIX_STYLE_VIRTUAL_DOMAINS' => "'False'",
+        'DEFAULT_SUBJECT_PREFIX' => "''",
+        'DEFAULT_REPLY_GOES_TO_LIST' => '1',
+      }}}
+
+    it do
+      should contain_package('mailman')
+
+      should contain_class('mailman')
+
+      should contain_service('mailman')
+     end
+
+  end
+end
+
